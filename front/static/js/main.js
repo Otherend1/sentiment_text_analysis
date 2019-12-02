e3453727ba170c40b923020fb5b712b807d4f88d
Tadouard <tadoudard@aneo.fr>
2019-12-02 12:04:12 +0100

    REFAC: html

--- front/static/js/main.js
+++ front/static/js/main.js
@@ -12,23 +12,57 @@ const interval = setInterval(function() {
 },1000);
 
 function update_bar(new_width) {
-  var elem = document.getElementById("bar");
-  var width = document.getElementById("bar").style.width;
-  width = width.replace("%", "");
-  var id = setInterval(frame, 20);
+  var container1 = document.getElementById("bar1_container");
+  var elem1 = document.getElementById("bar1");
+  var width1 = document.getElementById("bar1").style.width;
+  width1 = width1.replace("%", "");
+	
+  var container2 = document.getElementById("bar2_container");
+  var elem2 = document.getElementById("bar2");
+  var width2 = document.getElementById("bar2").style.width;
+  width2 = width2.replace("%", "");
+  
+  var width = get_width(width1, width2);  
+	
+  var id = setInterval(frame, 5);
   function frame() {
     if (width == new_width) {
       clearInterval(id);
     }
-    if (width < new_width) {
-      width++;
-      elem.style.width = width + '%';
-      elem.innerHTML = width * 1  + '%';
-    }
-    if (width > new_width) {
-      width--;
-      elem.style.width = width + '%';
-      elem.innerHTML = width * 1  + '%';
+    if (new_width>=50) {
+      if (width2==100) {
+        if (width1 < 2*(new_width - 50)) {
+          width1++;
+          elem1.style.width = width1 + '%';
+        }
+        if (width1 > 2*(new_width - 50)) {
+          width1--;
+          elem1.style.width = width1 + '%';
+        }           
+      } else {
+          width2++;
+          elem2.style.width = width2 + '%';
+      }
+  } else {
+      if (width1==0) {
+        if (100-width2 < 2*(50 - new_width)) {
+          width2--;
+          elem2.style.width = width2 + '%';
+        }
+        if (100-width2 > 2*(50 - new_width)) {
+          width2++;
+          elem2.style.width = width2 + '%';
+        }
+    } else {
+        width1--;
+        elem1.style.width = width1 + '%';
+      }
     }
   }
+}	
+
+function get_width(width1, width2) {
+  return(100-width1 + width2);
 }
+
+
